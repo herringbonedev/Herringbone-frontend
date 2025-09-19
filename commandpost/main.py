d9d90355410bb71e213ea034c5ccaca345c9dc5e
unixv <andrew@herringbone.dev>
2025-09-18 21:47:42 -0500

    init

---
+++ commandpost/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import requests
+import uvicorn
+
+LOGS_API_ENDPOINT = os.environ.get("LOGS_API_ENDPOINT")
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    # Load the latest logs
+    logs_result = requests.get(LOGS_API_ENDPOINT)
+    logs_result = logs_result.content()
+
+    return templates.TemplateResponse("index.html", {"request": request, "logs_result": logs_result})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
